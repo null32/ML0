0e0ed1dba2451aed84d6f8f35eca509fd17bc3b8
null32 <pavelgurwork@gmail.com>
2019-09-19 09:40:08 +0300

    kNN algo: the LOO(k) continuation

--- lab1/lab1.R
+++ lab1/lab1.R
@@ -3,8 +3,9 @@ dist <- function(a, b) {
   return (sqrt(sum( (a - b) ^ 2)))
 }
 
-# 1NN version 2
-knn <- function(dat, p, k = 6) {
+# kNN algo
+knn <- function(dat, p, k = c(6)) {
+  # calculate distances to each node in data
   dists <- vector("list", length(dat[[1]]))
   for (i in 1:length(dat[[1]])) {
     dists[[i]] <- dist(dat[i,][1:2], p)
@@ -14,25 +15,63 @@ knn <- function(dat, p, k = 6) {
   dat <- data.frame(dat, "Distance" = unlist(dists))
   # sort data by distance
   dat <- dat[order(dat$Distance),]
-  # take first k values from data
-  dat <- head(dat, k)
-  # count occurances of each group
-  res <- summary(dat$Species)
+
+  res <- list()  
+  for (i in seq(k)) {
+    # take first k values from data
+    datK <- head(dat, i)
+    # count occurances of each group
+    occs <- summary(datK$Species)
+    # most occuring group  
+    res[i] <- names(sort(occs, decreasing = TRUE))[1]
+  }
   
-  # most occuring group
-  return (names(sort(res, decreasing = TRUE))[1])
+  return (unlist(res))
 }
 
-# get data and plot it
-dat <- iris[3:5]
-colors <- c("setosa" = "red", "versicolor" = "green", "virginica" = "blue")
-plot(dat[1:2], pch = 21, col = colors[dat$Species], bg = colors[dat$Species])
-
-# classify N random points
-N = 10
-points <- data.frame("x" = runif(N, 1, 7), "y" = runif(N, 0, 2.5))
-for (i in seq(10)) {
-  p <- points[i, ]
-  cl <- knn(dat, p)
-  points(p[1], p[2], pch = 22, col = colors[cl], bg = colors[cl])
+# plot LOO(k)
+looFromK <- function(dat, algo, k) {
+  res <- loocv(dat, algo, k)
+  plot(data.frame("k"=k, "LOO(k)"=res), type="l")
 }
+
+# Perform LOOCV on 'algo' with 'dat' 
+loocv <- function(dat, algo, k) {
+  l <- length(dat[[1]])
+  correct <- rep(0, length(k))
+  
+  for (i in seq(l)) {
+    trainData <- dat[-i, ]
+    control <- dat[i, ]
+    
+    res <- algo(trainData, control[1:2], k)
+    correct <- correct + (res != control$Species)
+  }
+  
+  return (correct/l)
+}
+
+# example of usage
+demo <- function() {
+  # get data and plot it
+  dat <- iris[3:5]
+  colors <- c("setosa" = "red", "versicolor" = "green", "virginica" = "blue")
+  plot(dat[1:2], pch = 21, col = colors[dat$Species], bg = colors[dat$Species])
+  
+  # classify N random points
+  N = 10
+  points <- data.frame("x" = runif(N, 1, 7), "y" = runif(N, 0, 2.5))
+  for (i in seq(10)) {
+    p <- points[i, ]
+    cl <- knn(dat, p)
+    points(p[1], p[2], pch = 22, col = colors[cl], bg = colors[cl])
+  }
+}
+
+#res <- knn(iris[3:5], c(2, 2), k=1:10)
+#print(res)
+
+#res <- loocv(iris[3:5], knn, k=1:150)
+#print(res)
+
+looFromK(iris[3:5], knn, seq(150))
