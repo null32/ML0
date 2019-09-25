0cd54dc04012c0da318647b4cef4a3c32856920b
Pavel <pavelgurwork@gmail.com>
2019-09-26 01:25:53 +0300

    Fixed knn, added kwnn, updated readme

--- lab1/README.md
+++ lab1/README.md
@@ -1,4 +1,86 @@
-# Алгоритм ближайших соседей (NN)
-1. Алгоритм kNN
-![Пример работы](/lab1/knn.png)
+# Алгоритм ближайших соседей (kNN)
+## Алгоритм kNN
+**Алгоритм kNN** - метрический алгоритм классификации, основанный на оценивании сходства объектов. Классифицируемый объект относится к тому классу, которому принадлежат ближайшие к нему объекты обучающей выборки.
 
+### Формула алгоритма kNN выглядит следующим образом
+![Формула](knntex.png)\
+где *k* - параметр
+
+### Реализация алгоритма на языке R
+```r
+knn <- function(dat, p, k = c(6)) {
+  # calculate distances to each node in data
+  dists <- vector("list", length(dat[[1]]))
+  for (i in 1:length(dat[[1]])) {
+    dists[[i]] <- dist(dat[i,][1:2], p)
+  }
+  
+  # add distance to initial data
+  dat <- data.frame(dat, "Distance" = unlist(dists))
+  # sort data by distance
+  dat <- dat[order(dat$Distance),]
+
+  res <- list()  
+  for (i in seq(length(k))) {
+    # take first k values from data
+    datK <- head(dat, k[i])
+    # count occurances of each group
+    occs <- summary(datK$Species)
+    # most occuring group  
+    res[i] <- names(sort(occs, decreasing = TRUE))[1]
+  }
+  
+  return (unlist(res))
+}
+```
+### Выберем оптимальноё *k*, воспользовавшись критерием скользящего контроля LOO
+![График LOO(k)](LOOfromk.png)
+#### Таким образом оптимальное k=6
+## Карта классификации kNN
+![Карта классификации kNN](knn6map.png)
+
+## Алгоритм kwNN
+При взвешенном способе во внимание принимается не только количество попавших в область определённых классов, но и их удалённость от нового значения. Для каждого класса определяется оценка близости. У какого класса выше значение близости, тот класс и присваивается новому объекту.
+
+### Формула алгоритма kNN выглядит следующим образом
+![Формула](kwnntex.png)\
+где **w(i) = q^i** - геометрическая прогрессия с параметром q
+
+### Реализация алгоритма на языке R
+```r
+kwnn <- function(dat, p, k=6, q = c(0.8)) {
+  # calculate distances to each node in data
+  dists <- vector("list", length(dat[[1]]))
+  for (i in 1:length(dat[[1]])) {
+    dists[[i]] <- dist(dat[i,][1:2], p)
+  }
+  
+  # add distance to initial data
+  dat <- data.frame(dat, "Distance" = unlist(dists))
+  # sort data by distance
+  dat <- dat[order(dat$Distance),]
+  
+  res <- list()
+  # take first k values from data
+  datK <- head(dat, k)
+  
+  freq <- as.list(rep(0, length(levels(dat$Species))))
+  names(freq) = levels(dat$Species)
+  
+  for (j in seq(length(q))) {
+    for (l in seq(k)) {
+      e <- datK[l,]
+      freq[[e$Species]] <- freq[[e$Species]] + q[j] ^ l
+    }
+    # most occuring group  
+    res[j] <- names(sort(unlist(freq), decreasing = TRUE))[1]
+  }
+  
+  return (unlist(res))
+}
+```
+### Выберем оптимальноё *q*, воспользовавшись критерием скользящего контроля LOO
+![График LOO(q)](LOOfromq.png)
+#### Таким образом оптимальное k=0.8
+## Карта классификации kNN
+![Карта классификации kNN](kwnn608map.png)
--- lab1/lab1.R
+++ lab1/lab1.R
@@ -17,9 +17,9 @@ knn <- function(dat, p, k = c(6)) {
   dat <- dat[order(dat$Distance),]
 
   res <- list()  
-  for (i in seq(k)) {
+  for (i in seq(length(k))) {
     # take first k values from data
-    datK <- head(dat, i)
+    datK <- head(dat, k[i])
     # count occurances of each group
     occs <- summary(datK$Species)
     # most occuring group  
@@ -29,14 +29,8 @@ knn <- function(dat, p, k = c(6)) {
   return (unlist(res))
 }
 
-# plot LOO(k)
-looFromK <- function(dat, algo, k) {
-  res <- loocv(dat, algo, k)
-  plot(data.frame("k"=k, "LOO(k)"=res), type="l")
-}
-
 # Perform LOOCV on 'algo' with 'dat' 
-loocv <- function(dat, algo, k) {
+loocv1 <- function(dat, algo, k) {
   l <- length(dat[[1]])
   correct <- rep(0, length(k))
   
@@ -51,27 +45,114 @@ loocv <- function(dat, algo, k) {
   return (correct/l)
 }
 
+# plot LOO(k)
+looFromK <- function(dat, algo, k) {
+  res <- loocv1(dat, algo, k)
+  lfromk <- data.frame("k"=k, "LOO"=res)
+  plot(lfromk, type="l")
+  
+  # best k with lowest Q
+  m = lfromk[which.min(lfromk$LOO),]
+  points(m, pch=21, bg="green")
+  
+  return (m)
+}
+
+# kwNN algo
+kwnn <- function(dat, p, k=6, q = c(0.8)) {
+  # calculate distances to each node in data
+  dists <- vector("list", length(dat[[1]]))
+  for (i in 1:length(dat[[1]])) {
+    dists[[i]] <- dist(dat[i,][1:2], p)
+  }
+  
+  # add distance to initial data
+  dat <- data.frame(dat, "Distance" = unlist(dists))
+  # sort data by distance
+  dat <- dat[order(dat$Distance),]
+  
+  res <- list()
+  # take first k values from data
+  datK <- head(dat, k)
+  
+  freq <- as.list(rep(0, length(levels(dat$Species))))
+  names(freq) = levels(dat$Species)
+  
+  for (j in seq(length(q))) {
+    for (l in seq(k)) {
+      e <- datK[l,]
+      freq[[e$Species]] <- freq[[e$Species]] + q[j] ^ l
+    }
+    # most occuring group  
+    res[j] <- names(sort(unlist(freq), decreasing = TRUE))[1]
+  }
+  
+  return (unlist(res))
+}
+
+# Perform LOOCV on 'algo' with 'dat' 
+loocv2 <- function(dat, algo, q) {
+  l <- length(dat[[1]])
+  correct <- rep(0, length(q))
+  
+  for (i in seq(l)) {
+    trainData <- dat[-i, ]
+    control <- dat[i, ]
+    
+    res <- algo(trainData, control[1:2], q=q)
+    correct <- correct + (res != control$Species)
+  }
+  
+  return (correct/l)
+}
+
+# plot LOO(k)
+looFromW <- function(dat, algo, q) {
+  res <- loocv2(dat, algo, q)
+  print(res)
+  lfromk <- data.frame("q"=q, "LOO"=res)
+  plot(lfromk, type="l")
+  
+  # best k with lowest Q
+  m = lfromk[which.min(lfromk$LOO),]
+  points(m, pch=21, bg="green")
+  
+  return (m)
+}
+
 # example of usage
-demo <- function() {
+demo <- function(algo) {
   # get data and plot it
   dat <- iris[3:5]
   colors <- c("setosa" = "red", "versicolor" = "green", "virginica" = "blue")
   plot(dat[1:2], pch = 21, col = colors[dat$Species], bg = colors[dat$Species])
   
-  # classify N random points
-  N = 10
-  points <- data.frame("x" = runif(N, 1, 7), "y" = runif(N, 0, 2.5))
-  for (i in seq(10)) {
-    p <- points[i, ]
-    cl <- knn(dat, p)
-    points(p[1], p[2], pch = 22, col = colors[cl], bg = colors[cl])
+  # classification map
+  for (i in seq(1.0, 7.0, 0.1)) {
+    for (j in seq(0.1, 2.5, 0.1)) {
+      cl <- algo(dat, c(i, j))
+      points(i, j, pch = 21, col = colors[cl])
+    }
   }
 }
 
-#res <- knn(iris[3:5], c(2, 2), k=1:10)
+#demo(knn)
+#demo(kwnn)
+
+#res <- knn(iris[3:5], c(5, 1.5))
 #print(res)
 
-#res <- loocv(iris[3:5], knn, k=1:150)
+#res <- loocv1(iris[3:5], knn, k=1:150)
 #print(res)
 
-looFromK(iris[3:5], knn, seq(150))
+#res <- looFromK(iris[3:5], knn, 1:150)
+#print(res)
+
+#res <- kwnn(iris[3:5], c(5, 1.5), w=c(0.5, 0.6))
+#print(res)
+
+#res <- loocv2(iris[3:5], kwnn, q=seq(0.05, 1, 0.05))
+#print(res)
+
+res <- looFromW(iris[3:5], kwnn, q=seq(0.1, 1, 0.05))
+print(res)
