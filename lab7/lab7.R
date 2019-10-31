25b4e07a2a1aee9fafed399b1e1c14e63a97605b
null32 <pavelgurwork@gmail.com>
2019-10-31 09:29:32 +0300

    ya ni4ego ne ponimau

--- lab7/lab7.R
+++ lab7/lab7.R
@@ -2,7 +2,7 @@ library("MASS")
 
 n <- 100
 sigma1 <- matrix(c(1,0,0,1), 2, 2)
-sigma2 <- matrix(c(0.5, 0, 0, 0.5), 2, 2)
+sigma2 <- matrix(c(2, 0, 0, 0.4), 2, 2)
 
 mu1 <- c(0,0)
 mu2 <- c(2, 4)
@@ -22,22 +22,62 @@ points(xc2, pch=21, col=colors[2], bg=colors[2])
 
 estimateMu <- function(xs) {
   l <- dim(xs)[2]
-  res <- array(NA, l)
+  res <- matrix(NA, 1, l)
   for (i in seq(l)) {
-    res[i] <- mean(xs[,i])
+    res[1, i] <- mean(xs[,i])
   }
   return(res)
 }
 
 estimateSigma <- function(xs, mu) {
-  rows <- dims(xs)[1]
-  cols <- dims(xs)[2]
+  rows <- dim(xs)[1]
+  cols <- dim(xs)[2]
   
   res <- matrix(0, cols, cols)
   for (i in seq(rows)) {
-    res <- res + (xs[1,] - mu) %*% t(xs[1,] - mu)
+    res <- res + t(xs[1,] - mu) %*% (xs[1,] - mu)
   }
   
   return(res/(rows - 1))
 }
 
+getFunc <- function(sigma1, mu1, sigma2, mu2) {
+  d1 <- det(sigma1)
+  d2 <- det(sigma2)
+  invs1 <- solve(sigma1)
+  invs2 <- solve(sigma2)
+  
+  a <- sigma1 - sigma2
+  b <- sigma1 %*% t(mu1) - sigma2 %*% t(mu2)
+  
+  A <- a[1,1]
+  B <- a[2,2]
+  C <- a[1, 2] + a[2, 1]
+  D <- c*m2 + b*m2 - 2*d*m1 - f*m4 - g*m4 + 2*h*m3
+  E <- c*m1 + b*m1 - 2*a*m2 - f*m3 - g*m3 + 2*e*m4
+  G <- mu1 %*% solve(sigma1) %*% t(mu1) - mu2 %*% solve(sigma2) %*% t(mu2)
+  G1 <- d*m1^2 - c*m1*m2 - b*m1*m2 + a*m2^2 - h*m3^2 + g*m3*m4 + f*m3*m4 - e*m4^2
+  
+  func <- function(x, y) {
+    x^2 * A + y^2 * B + x*y*C + x*D + y*E + G
+  }
+  
+  return(func)
+}
+
+mu1 <- estimateMu(xc1)
+mu2 <- estimateMu(xc2)
+sigma1 <- estimateSigma(xc1, mu1)
+sigma2 <- estimateSigma(xc2, mu2)
+
+print(mu1)
+print(sigma1)
+print(mu2)
+print(sigma2)
+
+func <- getFunc(sigma1, mu1, sigma2, mu2)
+
+x <- seq(-10, 10)
+y <- seq(-10, 10)
+z <- outer(x, y, func)
+contour(x, y, z, levels = 0, lwd = 1, add = TRUE)
