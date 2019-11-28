c6b882a926ab53a26f8882882f869edba965545f
Pavel <pavelgurwork@gmail.com>
2019-11-28 08:24:36 +0300

    LDF fixes, broken in prev commit

--- lab8/lab8.R
+++ lab8/lab8.R
@@ -4,7 +4,7 @@ n <- 300
 sigma1 <- matrix(c(1, 0, 0, 10), 2, 2)
 
 mu1 <- c(0, 10)
-mu2 <- c(10, 10)
+mu2 <- c(10, 0)
 
 xc1 <- mvrnorm(n=n, mu = mu1, Sigma = sigma1)
 xc2 <- mvrnorm(n=n, mu = mu2, Sigma = sigma1)
@@ -13,7 +13,7 @@ plotxmin <- min(xc1[,1], xc2[,1]) - 1
 plotymin <- min(xc1[,2], xc2[,2]) - 1
 plotxmax <- max(xc1[,1], xc2[,1]) + 1
 plotymax <- max(xc1[,2], xc2[,2]) + 1
-plot(c(), type="n", xlab = "x", ylab = "y", xlim=c(plotxmin, plotxmax), ylim = c(plotymin, plotymax))
+plot(c(), type="n", xlab = "x", ylab = "y", xlim=c(plotxmin, plotxmax), ylim = c(plotymin, plotymax), main="Линейный дискриминат Фишера")
 
 colors <- c("magenta", "cyan")
 points(xc1, pch=21, col=colors[1], bg=colors[1])
@@ -26,6 +26,22 @@ estimateMu <- function(xs) {
     res[1, i] <- mean(xs[,i])
   }
   return(res)
+}
+
+estimateSigma <- function(xs1, mu1, xs2, mu2) {
+  rows1 <- dim(xs1)[1]
+  cols <- dim(xs1)[2]
+  rows2 <- dim(xs2)[1]
+  
+  res <- matrix(0, cols, cols)
+  for (i in seq(rows1)) {
+    res <- res + t(xs1[i,] - mu1) %*% (xs1[i,] - mu1)
+  }
+  for (i in seq(rows2)) {
+    res <- res + t(xs2[i,] - mu2) %*% (xs2[i,] - mu2)
+  }
+  
+  return(res/(rows1 + rows2 + 2))
 }
 
 getFunc <- function(sigma1, mu1, mu2) {
